1692e0ba4fa7c28856a7b622fe1409a4b90210fc
Reper2 <87633097+Reper2@users.noreply.github.com>
2022-05-29 00:20:04 +1000

    update console messages

--- scripts/music.js
+++ scripts/music.js
@@ -12,14 +12,14 @@ function audctrl_show() {
 	$('#music').show();
 	$('#audctrlBtn_show').hide();
 	$('#audctrlBtn_hide').show();
-	console.log('Showing audio controls');
+	console.log('Showing audio controls.');
 }
 
 function audctrl_hide() {
 	$('#music').hide();
 	$('#audctrlBtn_show').show();
 	$('#audctrlBtn_hide').hide();
-	console.log('Hiding audio controls');
+	console.log('Hiding audio controls.');
 }
 
 /**
